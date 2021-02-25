b17d3669445943a3e923f9b150729c4a5559b8ab
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-25 15:31:26 -0800

    created product index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
-const Product = require('./models/product')
+const Product = require("./models/product");
 
 mongoose
   .connect("mongodb://localhost:27017/farmStand", {
@@ -21,8 +21,9 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/dog", (req, res) => {
-  res.send("woof");
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
 });
 
 app.listen(7001, () => {
