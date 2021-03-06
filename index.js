9e18038e54540542e27b8c8f8107abc087dbb124
cohlerCoded <jeffcohler@cohlercoded.com>
2021-03-06 13:31:01 -0800

    differentiated mongoose errors

--- index.js
+++ index.js
@@ -107,6 +107,17 @@ app.delete(
   })
 );
 
+const handleValidationError = (err) => {
+  console.dir(err);
+  return new AppError(`Validation Failed... ${err.message}`, 400);
+};
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.name === "ValidationError") err = handleValidationError(err);
+  next(err);
+});
+
 app.use((err, req, res, next) => {
   const { message = "OOPS ERROR", status = 500 } = err;
   res.status(status).send(message);
